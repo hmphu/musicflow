52f035a5e30f3a156907a6e679322aa80a827e8f
Phu Hoang <2835749+hmphu@users.noreply.github.com>
2026-06-18 11:18:18 +0700

    Add DEBUG env variable support for debug mode

--- app.py
+++ app.py
@@ -223,4 +223,4 @@ def playlist():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("DEBUG", "false").lower() == "true")
